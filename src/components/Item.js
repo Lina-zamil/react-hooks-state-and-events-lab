00999dd109608430e35c0ad06545b1af92fe799f
lina abumahfood <lina.abumahfood@gmail.com>
2023-11-09 23:10:10 +0300

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false)
+  const className =isInCart?"in-cart":"";
+  const buttonClassName=isInCart?"remove": "add"
+  const addRemoveFromCart=isInCart?"revome from Cart":"Add to Cart";
+  const handleCart=()=>setIsInCart(!isInCart)
+    
   return (
-    <li className="">
+    <li className={className}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClassName} onClick={handleCart} >{addRemoveFromCart}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+const[selectCategory,setSelectCategory]=useState("All");
+
+function handleChangeCategory(el){
+  setSelectCategory(el.target.value)
+}
+const filteredList=items.filter((item)=>{
+if(selectCategory==="All")return true
+else
+return item.category===selectCategory
+});
+ 
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChangeCategory}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +24,12 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+       {filteredList.map(
+            (item) => <Item key={item.id} name={item.name} category={item.category} />)}
       </ul>
     </div>
   );
 }
 
 export default ShoppingList;
+ 
